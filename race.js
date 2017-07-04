7b7ea944544665e3cba4e7d37981bf218c2ddf33
GeordieP <geordie.powers@gmail.com>
2017-07-03 22:01:23 -0400

    Race now keeps track of its current state using a set of pre-defined states.

--- race.js
+++ race.js
@@ -2,6 +2,13 @@
 
 const Entrant = require('./entrant')
 
+const states = {
+    IDLE: 'IDLE',
+    COUNTDOWN: 'COUNTDOWN',
+    UNDERWAY: 'UNDERWAY',
+    COMPLETE: 'COMPLETE'
+}
+
 let START_COUNTDOWN_LENGTH = 30   // seconds
 const CHANNEL_DELETE_TIMEOUT = 300000       // 5 minutes
 
@@ -44,9 +51,9 @@ module.exports = function(creator_guildmember, game_name) {
     this.entrants = {}
     this.finishers = []
     this.race_start_time = -1
+    this.state = states.IDLE    // initial state
 
     api.game = game_name
-    // TODO: replace spaces in race_name with underscores for racename
     api.name = api.game + "_" + Date.now()
     api.creator_guildmember = creator_guildmember
 
@@ -70,7 +77,7 @@ module.exports = function(creator_guildmember, game_name) {
     }
 
     api.delete = (timeout = CHANNEL_DELETE_TIMEOUT) => {
-        if (this.race_start_time !== -1) {
+        if (this.state === states.COUNTDOWN || this.state === states.UNDERWAY) {
             return Promise.reject("Race is in progress")
         }
         
@@ -106,8 +113,11 @@ module.exports = function(creator_guildmember, game_name) {
     }
 
     api.start = () => {
-        if (this.start_countdown_timer !== null) {
-            return
+        switch(true) {
+            case (this.start_countdown_timer !== null):       // if a countdown timer is already counting down
+            case (this.state === states.COUNTDOWN):           // check states
+            case (this.state === states.UNDERWAY):
+                return Promise.reject()
         }
 
         this.start_countdown_timer_value = START_COUNTDOWN_LENGTH
@@ -122,7 +132,9 @@ module.exports = function(creator_guildmember, game_name) {
         // send warning message
         this.channel.sendMessage("__**Race beginning in " + this.start_countdown_timer_value + " seconds!**__")
 
+        this.state = states.COUNTDOWN
         this.start_countdown_timer = setInterval(COUNTDOWN_FUNC, 1000)
+
     }
 
     const COUNTDOWN_FUNC = () => {
@@ -131,12 +143,13 @@ module.exports = function(creator_guildmember, game_name) {
         if (this.start_countdown_timer_value == 0) {
             this.race_start_time = Date.now()
             this.channel.sendMessage(GO_MSG)
-        } else if (this.start_countdown_timer_value <= -5) {
-            // we've gone past 0, kill the timer and reset everything
+
+            // kill the timer and reset everything
+            this.state = states.UNDERWAY
             clearInterval(this.start_countdown_timer)
             this.start_countdown_timer = null
             sent_countdown_msg_ref = null
-        } else if (this.start_countdown_timer_value <= 3 && this.start_countdown_timer_value > 0) {
+       } else if (this.start_countdown_timer_value <= 3 && this.start_countdown_timer_value > 0) {
             // discord has a message send rate cap
             // instead of sending a message for each countdown tick, edit the existing one
             if (sent_countdown_msg_ref == null) {
@@ -160,8 +173,7 @@ module.exports = function(creator_guildmember, game_name) {
             msg.push("**[" + entrant.finish_place() + "]** " + entrant.name + " - `" + entrant.formatted_finish_time() + "`")
         }
 
-        // reset start time so we can tell the race is over
-        this.race_start_time = -1
+        this.state = states.COMPLETE
 
         return this.channel.sendMessage(msg)
     }
@@ -170,7 +182,7 @@ module.exports = function(creator_guildmember, game_name) {
         // handle illegal cases
         switch(true) {
             case (this.entrants.hasOwnProperty(guildmember.id)):        // user is already entered
-            case (this.race_start_time !== -1):                         // race is in progress
+            case (this.state === states.UNDERWAY):                      // race is in progress
                 return Promise.reject()
         }
 
@@ -221,7 +233,7 @@ module.exports = function(creator_guildmember, game_name) {
         switch(true) {
             case (!this.entrants.hasOwnProperty(guildmember.id)):
                 return Promise.reject("User is not in this race.")
-            case (this.race_start_time != -1):                          // race is in progress
+            case (this.state === states.UNDERWAY):                  // race is in progress
                 return Promise.reject()
         }
 
@@ -235,7 +247,7 @@ module.exports = function(creator_guildmember, game_name) {
             case (!this.entrants.hasOwnProperty(guildmember.id)):
                 return Promise.reject("User is not in this race.")
             case (this.entrants[guildmember.id].is_ready()):        // user is already ready
-            case (this.race_start_time != -1):                      // race is in progress
+            case (this.state === states.UNDERWAY):                  // race is in progress
                 return Promise.reject()
         }
 
@@ -249,7 +261,7 @@ module.exports = function(creator_guildmember, game_name) {
                 return Promise.reject("User is not in this race.")
             case (!this.entrants[guildmember.id].is_ready()):       // user is already not ready
             case (this.entrants[guildmember.id].is_done()):         // user is done
-            case (this.race_start_time !== -1):                     // race is in progress
+            case (this.state === states.UNDERWAY):                  // race is in progress
                 return Promise.reject()
         }
 
@@ -282,8 +294,12 @@ module.exports = function(creator_guildmember, game_name) {
 
         // handle illegal cases
         switch(true) {
-            case (this.race_start_time === -1):
+            case (this.state === states.IDLE):
                 return Promise.reject("Race was never started, or encountered an error")
+            case (this.state === states.COUNTDOWN):
+                return Promise.reject("Race hasn't started yet.")
+            case (this.state === states.COMPLETE):
+                return Promise.reject("Race is already complete.")
             case (!this.entrants.hasOwnProperty(guildmember.id)):
                 return Promise.reject("User appears to not have been in the race, an error may have occurred. Finish time was `" + formatted_finish_time + "`")
             case (this.entrants[guildmember.id].is_done()):
